9d19b13ef11425ac78ab5a330396b7eb813e3437
lbugnion <laurent@galasoft.ch>
2019-01-06 14:28:06 +0800

    First commit with POST error (bad request / 400)

---
+++ TestPost/Pages/Index.cshtml.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace TestPost.Pages
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+
+        }
+
+        public void OnPost()
+        {
+
+        }
+    }
+}
